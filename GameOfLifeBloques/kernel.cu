164b1ce3b08d74be639aec813c4a77f2461ca6f9
Alberto González Martínez <albergonmar@gmail.com>
2020-03-03 22:25:03 +0100

    Puta zorra bloques casi terminado
    -Hay que controlar de alguna manera lo de que la matriz sea impar
    -Ver el TILE_WIDTH en funcion de la gpu que lo ejecute
    -Poner algun mensaje de error más para que no pete la ejecución de kernel
    -Tanto en bloque como en la anterior hacer una función o algo la cual en función de la dimension de la matriz tenga más o menos random % numero que más o menos todas no tengan ni muchas casillas vivas ni muchas casillas muertas

--- GameOfLifeBloques/kernel.cu
+++ GameOfLifeBloques/kernel.cu
@@ -9,11 +9,17 @@
 #include <iostream>
 #include <stdlib.h>
 
-void imprimirMatriz(char** matriz, int fila, int columna);
+#define TILE_WIDTH 2
 
-void rellenarMatriz(char** matriz, int fila, int columna);
+void imprimirMatriz(char* matriz, int dimension, int columna);
 
-int contarVivas(char** matriz, int fila, int columna);
+void rellenarMatriz(char* matriz, int dimension);
+
+int contarVivas(char* matriz, int dimension);
+
+cudaError_t lanzarKernel(char* matriz, char* matrizResultado, int fila, int columna);
+
+__global__ void movimientoCelularBloque(char* matriz, char* matrizResultado, int fila, int columna);
 
 
 int main(int arg, char* argv[])
@@ -39,17 +45,11 @@ int main(int arg, char* argv[])
         int dimension = columna * fila;
 
         //Matrices
-        char** matriz = NULL;
-        char** matrizResultado = NULL;
+        char* matriz = NULL;
+        char* matrizResultado = NULL;
 
-        matriz = (char**) malloc(sizeof(char) * dimension);
-        matrizResultado = (char**) malloc(sizeof(char) * dimension);
-
-        //Creamos las filas de las matrices
-        for (int i = 0; i < columna; i++) {
-            matriz[i] = (char*) malloc(sizeof(char) * columna);
-            matrizResultado[i] = (char*) malloc(sizeof(char) * columna);
-        }
+        matriz = (char*) malloc(sizeof(char) * dimension);
+        matrizResultado = (char*) malloc(sizeof(char) * dimension);
 
         //Booleano para saber si el usuario quiere manual o automatico, por defecto automatico
         bool manual = false;
@@ -72,11 +72,40 @@ int main(int arg, char* argv[])
             }
 
             //Rellenamos el tablero con celulas muertas y vivas
-            rellenarMatriz(matriz, fila, columna);
+            rellenarMatriz(matriz, dimension);
 
             printf("\n***TABLERO INICIAL***\n");
-            printf("Celulas vivas: %d\n", contarVivas(matriz, fila, columna));
-            imprimirMatriz(matriz, fila, columna);
+            imprimirMatriz(matriz, dimension, columna);
+
+            int generaciones = 1;
+            int vivas = 0;
+
+            while (vivas != dimension) {
+                 
+                system("CLS");
+
+                if (generaciones == 1) {
+                    lanzarKernel(matriz, matrizResultado, fila, columna);
+                }
+                else {
+                    lanzarKernel(matrizResultado, matrizResultado, fila, columna);
+                }
+
+                vivas = contarVivas(matrizResultado, dimension);
+
+                printf("\nGeneracion: %d\n", generaciones);
+                printf("Celulas vivas: %d\n", vivas);
+                imprimirMatriz(matrizResultado, dimension, columna);
+
+                if (manual) {
+                    system("pause");
+                }
+                else {
+                    Sleep(1000);
+                }
+
+                generaciones++;
+            }
         }
 
         //Liberamos los arrays
@@ -86,48 +115,350 @@ int main(int arg, char* argv[])
     }
 }
 
-void imprimirMatriz(char** matriz, int fila, int columna) {
+cudaError_t lanzarKernel(char* matriz, char* matrizResultado, int fila, int columna) {
 
-    for (int i = 0; i < fila; i++) {
-        for (int j = 0; j < columna; j++) {
-            printf(" %c ",matriz[i][j]);
-        }
+    //Punteros a las matrices que se meten por el kernel
+    char* matriz_d;
+    char* matrizResultado_d;
 
-        printf("\n");
+    int dimension = fila * columna;
+
+    cudaError_t cudaStatus;
+
+    //Dimension del bloque y grid
+    dim3 dimGrid(fila/TILE_WIDTH, columna/TILE_WIDTH);
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH);
+
+    //Seleccionamos el device
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice fallo: Tienes una GPU instalada?");
+        goto Error;
     }
+
+    //Reservamos las memorias
+    cudaStatus = cudaMalloc((void**)&matriz_d, dimension * sizeof(char));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: cudaMalloc matriz_d fallo.");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&matrizResultado_d, dimension * sizeof(char));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: cudaMalloc matrizResultado_d fallo.");
+        goto Error;
+    }
+
+    //Copiamos las matrices que entran por parametro
+    cudaStatus = cudaMemcpy(matriz_d, matriz, dimension * sizeof(char), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: cudaMemcpy matriz a matriz_d fallo.");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(matrizResultado_d, matrizResultado, dimension * sizeof(char), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: cudaMemcpy matrizResultado a matrizResultado_d fallo.");
+        goto Error;
+    }
+
+
+    //Lanzamos el kernel
+    movimientoCelularBloque <<< dimGrid, dimBlock >>> (matriz_d, matrizResultado_d, fila, columna);
+
+
+    //Miramos los errores al lanzar el kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: lanzamiento de kernel fallo: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+
+    //Miramos errores despues de lanzar el kernel
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: el kernel fallo con codigo %d\n", cudaStatus);
+        goto Error;
+    }
+
+    //Copiamos el resultado en nuestra matriz
+    cudaStatus = cudaMemcpy(matrizResultado, matrizResultado_d, dimension * sizeof(char), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "ERROR: cudaMemcpy matrizResultado_d a matrizResultado fallo.");
+        goto Error;
+    }
+
+
+Error:
+    cudaFree(matriz_d);
+    cudaFree(matrizResultado_d);
+
+    return cudaStatus;
 }
 
-void rellenarMatriz(char** matriz, int fila, int columna) {
+__global__ void movimientoCelularBloque(char* matriz, char* matrizResultado, int fila, int columna) {
 
-    srand(time(0));
+    int filaPos = blockIdx.x * blockDim.x + threadIdx.x;
+    int columnaPos = blockIdx.y * blockDim.y + threadIdx.y;
 
-    for (int i = 0; i < fila; i++) {
-        for (int j = 0; j < columna; j++) {
+    int posicion = filaPos * columna + columnaPos;
 
-            int random = rand() % (fila*columna) + 1;
-
-            if (random % 3 == 0 && random % 2 == 0) {
-
-                matriz[i][j] = 'X';
-            }
-            else {
-                matriz[i][j] = 'O';
-            }
-        }
-    }
-}
-
-int contarVivas(char** matriz, int fila, int columna) {
+    //printf("BLOCK[%dx%d] DIM[%dx%d] POSICION[%dx%d] -> %d\n", blockIdx.x, blockIdx.y, blockDim.x, blockDim.y, threadIdx.x, threadIdx.y,posicion);
 
     int contador = 0;
 
-    for (int i = 0; i < fila; i++) {
-        for (int j = 0; j < columna; j++) {
-            if (matriz[i][j] == 'X') {
-                contador++;
+    //Primera fila 0x
+    if (filaPos == 0) {
+        //Posicion esquina ariba izquierda 0x0
+        if (columnaPos == 0) {
+
+            if ((matriz[posicion + 1]) == 'X') { contador++; }
+            if ((matriz[posicion + columna]) == 'X') { contador++; }
+            if ((matriz[posicion + (columna + 1)]) == 'X') { contador++; }
+
+            //VIVA
+            if (matriz[posicion] == 'X') {
+
+                if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
             }
+            //MUERTA
+            else {
+
+                if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+        }
+        //Posicion esquina superior derecha
+        else if (columnaPos == (columna - 1)) {
+
+            if ((matriz[posicion - 1]) == 'X') { contador++; }
+            if ((matriz[posicion + columna]) == 'X') { contador++; }
+            if ((matriz[posicion + (columna - 1)]) == 'X') { contador++; }
+
+            //VIVA
+            if (matriz[posicion] == 'X') {
+
+                if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+            //MUERTA
+            else {
+
+                if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+        }
+        //Posicion en la primera fila sin contar esquinas
+        else {
+
+            if ((matriz[posicion - 1]) == 'X') { contador++; }
+            if ((matriz[posicion + 1]) == 'X') { contador++; }
+            if ((matriz[posicion + columna]) == 'X') { contador++; }
+            if ((matriz[posicion + (columna - 1)]) == 'X') { contador++; }
+            if ((matriz[posicion + (columna + 1)]) == 'X') { contador++; }
+
+            //VIVA
+            if (matriz[posicion] == 'X') {
+
+                if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+            //MUERTA
+            else {
+
+                if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+        }
+    }
+    //****************************
+    //Ulima fila finalXx
+    else if (filaPos == (fila - 1)) {
+        //Posicion esquina abajo izquierda
+        if (columnaPos == 0) {
+
+            if ((matriz[posicion + 1]) == 'X') { contador++; }
+            if ((matriz[posicion - columna]) == 'X') { contador++; }
+            if ((matriz[posicion - (columna - 1)]) == 'X') { contador++; }
+
+            //VIVA
+            if (matriz[posicion] == 'X') {
+
+                if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+            //MUERTA
+            else {
+
+                if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+        }
+        //Posicion esquina abajo derecha
+        else if (columnaPos == (columna - 1)) {
+
+            if ((matriz[posicion - 1]) == 'X') { contador++; }
+            if ((matriz[posicion - columna]) == 'X') { contador++; }
+            if ((matriz[posicion - (columna + 1)]) == 'X') { contador++; }
+
+            //VIVA
+            if (matriz[posicion] == 'X') {
+
+                if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+            //MUERTA
+            else {
+
+                if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+        }
+        //Posiciones ultima fila entre esquinas
+        else {
+
+            if ((matriz[posicion - 1]) == 'X') { contador++; }
+            if ((matriz[posicion + 1]) == 'X') { contador++; }
+            if ((matriz[posicion - columna]) == 'X') { contador++; }
+            if ((matriz[posicion - (columna + 1)]) == 'X') { contador++; }
+            if ((matriz[posicion - (columna - 1)]) == 'X') { contador++; }
+
+            //VIVA
+            if (matriz[posicion] == 'X') {
+
+                if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+            //MUERTA
+            else {
+
+                if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+                else { matrizResultado[posicion] = 'O'; }
+            }
+        }
+    }
+    //****************************
+    //Primera columna entre las dos esquinas izquierdas
+    else if (columnaPos == 0) {
+
+        if ((matriz[posicion + 1]) == 'X') { contador++; }
+        if ((matriz[posicion - columna]) == 'X') { contador++; }
+        if ((matriz[posicion + columna]) == 'X') { contador++; }
+        if ((matriz[posicion + (columna + 1)]) == 'X') { contador++; }
+        if ((matriz[posicion - (columna - 1)]) == 'X') { contador++; }
+
+        //VIVA
+        if (matriz[posicion] == 'X') {
+
+            if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+            else { matrizResultado[posicion] = 'O'; }
+        }
+        //MUERTA
+        else {
+
+            if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+            else { matrizResultado[posicion] = 'O'; }
+        }
+    }
+    //****************************
+    //Ultima colunmna xfinalY
+    else if (columnaPos == columna - 1) {
+
+        if ((matriz[posicion - 1]) == 'X') { contador++; }
+        if ((matriz[posicion + columna]) == 'X') { contador++; }
+        if ((matriz[posicion - columna]) == 'X') { contador++; }
+        if ((matriz[posicion - (columna + 1)]) == 'X') { contador++; }
+        if ((matriz[posicion + (columna - 1)]) == 'X') { contador++; }
+
+        //VIVA
+        if (matriz[posicion] == 'X') {
+
+            if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+            else { matrizResultado[posicion] = 'O'; }
+        }
+        //MUERTA
+        else {
+
+            if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+            else { matrizResultado[posicion] = 'O'; }
+        }
+    }
+    //****************************
+    //Posiciones fuera de los margenes
+    else {
+
+        if ((matriz[posicion + 1]) == 'X') { contador++; }
+        if ((matriz[posicion - 1]) == 'X') { contador++; }
+        if ((matriz[posicion + columna]) == 'X') { contador++; }
+        if ((matriz[posicion - columna]) == 'X') { contador++; }
+        if ((matriz[posicion - (columna + 1)]) == 'X') { contador++; }
+        if ((matriz[posicion - (columna - 1)]) == 'X') { contador++; }
+        if ((matriz[posicion + (columna + 1)]) == 'X') { contador++; }
+        if ((matriz[posicion + (columna - 1)]) == 'X') { contador++; }
+
+        //VIVA
+        if (matriz[posicion] == 'X') {
+
+            if (contador >= 2) { matrizResultado[posicion] = 'X'; }
+            else { matrizResultado[posicion] = 'O'; }
+        }
+        //MUERTA
+        else {
+
+            if (contador >= 3) { matrizResultado[posicion] = 'X'; }
+            else { matrizResultado[posicion] = 'O'; }
+        }
+    }
+}
+
+void imprimirMatriz(char* matriz, int dimension, int columna) {
+
+    for (int i = 0; i < dimension; i++) {
+
+        if (matriz[i] == 'X') {
+            printf(" 0 ");
+        }
+        else {
+            printf(" . ");
+        }
+
+        if ((i + 1) % columna == 0) {
+            printf("\n");
+        }
+    }
+}
+
+int contarVivas(char* matriz, int dimension) {
+
+    int contador = 0;
+
+    for (int i = 0; i < dimension; i++) {
+        if (matriz[i] == 'X') {
+            contador++;
         }
     }
 
     return contador;
+}
+
+void rellenarMatriz(char* matriz, int dimension) {
+
+    srand(time(0));
+
+    for (int i = 0; i < dimension; i++) {
+
+        char* celula = matriz + i;
+
+        int random = rand() % dimension + 1;
+
+        if (random % 3 == 0 && random % 2 == 0) {
+
+            *celula = 'X';
+        }
+        else {
+            *celula = 'O';
+        }
+
+    }
 }
